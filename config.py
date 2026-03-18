c034adbba55e222ca852241ef2b8a5e997a8688b
FerAguayo <pxocktopus@gmail.com>
2026-03-18 10:07:52 +0100

    Esqueleto del proyecto

---
+++ config.py
@@ -0,0 +1,4 @@
+API_KEY = "bc42d386e17c4405a14a2e8146c4f4ae"
+API_URL_BASE = "https://pro-api.coinmarketcap.com/"
+DATA_ORIGIN = "data/coinmarket.sqlite"
+VALID_COINS = ["EUR", "BTC", "ETH", "USDT", "BNB", "XRP", "USDC", "SOL", "TRON", "DOGE"]
